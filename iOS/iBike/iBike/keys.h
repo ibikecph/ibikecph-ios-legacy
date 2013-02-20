0b3b71e0a7b16a9e75d5a9bf5aa928cf82289c5a
Ivan Pavlovic <ivan@spoiledmilk.com>
2013-02-20 15:05:38 +0100

    Added keys file

---
+++ iOS/iBike/iBike/keys.h
@@ -0,0 +1,11 @@
+//
+//  keys.h
+//  iBike
+//
+//  Created by Ivan Pavlovic on 20/02/2013.
+//  Copyright (c) 2013 Spoiled Milk. All rights reserved.
+//
+
+#define GOOGLE_ANALYTICS_KEY @"YOUR API KEY GOES HERE"
+#define GOOGLE_API_KEY @"YOUR API KEY GOES HERE"
+
