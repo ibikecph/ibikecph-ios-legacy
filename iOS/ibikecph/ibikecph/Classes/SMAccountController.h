c69cefa11a292c8f5bc8ade4f8add566d310a1e4
Ivan Pavlovic <ivan@spoiledmilk.com>
2013-03-21 16:19:57 +0100

    new design

---
+++ iOS/ibikecph/ibikecph/Classes/SMAccountController.h
@@ -0,0 +1,13 @@
+//
+//  SMAccountController.h
+//  I Bike CPH
+//
+//  Created by Ivan Pavlovic on 21/03/2013.
+//  Copyright (c) 2013 Spoiled Milk. All rights reserved.
+//
+
+#import "SMTranslatedViewController.h"
+
+@interface SMAccountController : SMTranslatedViewController
+
+@end
---
+++ iOS/ibikecph/ibikecph/Classes/SMAccountController.m
@@ -0,0 +1,38 @@
+//
+//  SMAccountController.m
+//  I Bike CPH
+//
+//  Created by Ivan Pavlovic on 21/03/2013.
+//  Copyright (c) 2013 Spoiled Milk. All rights reserved.
+//
+
+#import "SMAccountController.h"
+
+@interface SMAccountController ()
+
+@end
+
+@implementation SMAccountController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
