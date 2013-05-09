0e187a7b387ef3ce3b6fdfe9e955e75062b8ffae
Rasko Gojkovic <rasko@spoiledmilk.com>
2013-05-09 17:55:55 +0200

    Some UI changes

---
+++ iOS/ibikecph/ibikecph/Classes/SMLoginController.h
@@ -0,0 +1,13 @@
+//
+//  SMLoginController.h
+//  I Bike CPH
+//
+//  Created by Rasko Gojkovic on 5/9/13.
+//  Copyright (c) 2013 City of Copenhagen. All rights reserved.
+//
+
+#import "SMTranslatedViewController.h"
+
+@interface SMLoginController : SMTranslatedViewController
+
+@end
---
+++ iOS/ibikecph/ibikecph/Classes/SMLoginController.m
@@ -0,0 +1,38 @@
+//
+//  SMLoginController.m
+//  I Bike CPH
+//
+//  Created by Rasko Gojkovic on 5/9/13.
+//  Copyright (c) 2013 City of Copenhagen. All rights reserved.
+//
+
+#import "SMLoginController.h"
+
+@interface SMLoginController ()
+
+@end
+
+@implementation SMLoginController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
