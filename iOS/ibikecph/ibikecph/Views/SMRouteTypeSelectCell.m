5fb2fceb24d1fef9baf253db575e26c99d5c313a
Ivan Pavlovic <ivan@spoiledmilk.com>
2013-06-06 11:26:05 +0200

    change route type design implemented

---
+++ iOS/ibikecph/ibikecph/Views/SMRouteTypeSelectCell.m
@@ -0,0 +1,29 @@
+//
+//  SMRouteTypeSelectCell.m
+//  I Bike CPH
+//
+//  Created by Ivan Pavlovic on 06/06/2013.
+//  Copyright (c) 2013 City of Copenhagen. All rights reserved.
+//
+
+#import "SMRouteTypeSelectCell.h"
+
+@implementation SMRouteTypeSelectCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
